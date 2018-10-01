d2b553c6cc525503ccd732a6e9fb8ac048fceb5e
Miguel Soler <msoler735@gmail.com>
2018-10-01 17:26:24 -0500

    Register Styles Miguel

--- shopmium/functions.php
+++ shopmium/functions.php
@@ -28,37 +28,39 @@ function theme_enqueue_styles()
 
 	wp_enqueue_style('child-style', get_stylesheet_directory_uri() . '/style.css', $child_style, $resources_version);
 
+	wp_enqueue_style('ms-styles', get_stylesheet_directory_uri() . '/miguel.css', $child_style, $resources_version);
+
 }
 
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 
-  function footer_services_menu(){                                                     
-    wp_nav_menu([
-    'theme_location'  => 'footer-services',
-    'menu'            => '',
-    'container'       => 'div',
-    'container_class' => 'menu-{menu slug}-container',
-    'container_id'    => '',
-    'menu_class'      => 'menu',
-    'menu_id'         => '',
-    'echo'            => true,
-    'fallback_cb'     => 'wp_page_menu',
-    'before'          => '',
-    'after'           => '',
-    'link_before'     => '',
-    'link_after'      => '',
-    'items_wrap'      => '<ul>%3$s</ul>',
-    'depth'           => 0,
-    'walker'          => ''
+  function footer_services_menu(){
+	wp_nav_menu([
+	'theme_location'  => 'footer-services',
+	'menu'            => '',
+	'container'       => 'div',
+	'container_class' => 'menu-{menu slug}-container',
+	'container_id'    => '',
+	'menu_class'      => 'menu',
+	'menu_id'         => '',
+	'echo'            => true,
+	'fallback_cb'     => 'wp_page_menu',
+	'before'          => '',
+	'after'           => '',
+	'link_before'     => '',
+	'link_after'      => '',
+	'items_wrap'      => '<ul>%3$s</ul>',
+	'depth'           => 0,
+	'walker'          => ''
    ]);
   }
 
 
   function register_shopmium_menu()
 {
-    register_nav_menus(array( // Using array to specify more menus if needed     
-        'footer-services' => __('Footer services', 'uncode'), // Main Navigation
-    ));
+	register_nav_menus(array( // Using array to specify more menus if needed
+		'footer-services' => __('Footer services', 'uncode'), // Main Navigation
+	));
 }
 
  add_action('init', 'register_shopmium_menu');
