43ab6f1da769febbf623a4cf563cf007e9427983
Miguel Soler <msoler735@gmail.com>
2018-10-04 16:02:08 -0500

    Finished custom post downloads

--- shopmium/functions/miguel-functions.php
+++ shopmium/functions/miguel-functions.php
@@ -178,7 +178,7 @@ function download_shopmium_resources($atts){
                 'terms'     => array( sanitize_title( $atts['resource'] ) )
             ) )
         ) );?>
-        <div id="ms-container-resourced">
+        <div class="ms-container-resourced">
            <?php
 
             if ( $loop->have_posts() ) :
@@ -195,17 +195,19 @@ function download_shopmium_resources($atts){
 
                  if ($total_pages > 1){
 
-                     $current_page = max(1, get_query_var('paged'));
-
-                     echo paginate_links(array(
-                         'base' => get_pagenum_link(1) . '%_%',
-                         'format' => '/page/%#%',
-                         'current' => $current_page,
-                         'total' => $total_pages,
-                         'prev_text'    => __('« prev'),
-                         'next_text'    => __('next »'),
-                     ));
-                 }
+                     $current_page = max(1, get_query_var('paged'));?>
+					 <div class="ms-paginator-resources">
+	                     <?php
+	                     	  echo paginate_links(array(
+	                         'base' => get_pagenum_link(1) . '%_%',
+	                         'format' => '/page/%#%',
+	                         'current' => $current_page,
+	                         'total' => $total_pages,
+	                         'prev_text'    => __('« prev'),
+	                         'next_text'    => __('next »'),
+	                     ));?>
+					 </div>
+                 <?php }
             endif;
             wp_reset_postdata();?>
         </div>
